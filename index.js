c1242524cc0d4bad8ab040d17cbdcb6ee452f517
sohansshetty <sohansshetty1997@gmail.com>
2023-08-19 09:22:06 +0530

    2nd task

--- index.js
+++ index.js
@@ -47,6 +47,66 @@ app.post('/forget-password', (req, res) => {
   return res.status(200).json({ message: 'Password reset link sent to email' });
 });
 
+let posts = [];
+
+app.post('/posts', (req, res) => {
+  const { userId, content } = req.body;
+
+  if (!userId || !content) {
+    return res.status(400).json({ message: 'User ID and content are required' });
+  }
+
+  const newPost = {
+    postId: posts.length + 1,
+    userId,
+    content,
+    likes: 0,
+    comments: []
+  };
+
+  posts.push(newPost);
+
+  return res.status(201).json({ message: 'Post created successfully', post: newPost });
+});
+
+app.get('/posts', (req, res) => {
+  return res.status(200).json(posts);
+});
+
+app.put('/posts/:postId/like', (req, res) => {
+  const { postId } = req.params;
+
+  const post = posts.find(p => p.postId === parseInt(postId));
+
+  if (!post) {
+    return res.status(404).json({ message: 'Post not found' });
+  }
+
+  post.likes += 1;
+
+  return res.status(200).json({ message: 'Post liked', post });
+});
+
+app.post('/posts/:postId/comments', (req, res) => {
+  const { postId } = req.params;
+  const { userId, comment } = req.body;
+
+  const post = posts.find(p => p.postId === parseInt(postId));
+
+  if (!post) {
+    return res.status(404).json({ message: 'Post not found' });
+  }
+
+  const newComment = {
+    userId,
+    comment
+  };
+
+  post.comments.push(newComment);
+
+  return res.status(201).json({ message: 'Comment added', post });
+});
+
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
